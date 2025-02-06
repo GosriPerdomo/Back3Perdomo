2580418b272729eb308e9d95e5f99d5d6ec20131
GosriPerdomo <153382657+GosriPerdomo@users.noreply.github.com>
2025-02-06 01:58:32 -0300

    Update Notas
    Update en notaciones para dejar mas limpio

--- src/Utils/mocking.js
+++ src/Utils/mocking.js
@@ -1,5 +1,5 @@
 // utils/mocking.js
-const { faker } = require('@faker-js/faker');
+const { faker } = require('@faker-js/faker');  //faker.js para los datos ficticios 
 const Cart = require('../models/cart');
 
 async function generateMockUsers(numUsers = 0, numCarts = 0, isGetRequest = false) {
@@ -7,7 +7,7 @@ async function generateMockUsers(numUsers = 0, numCarts = 0, isGetRequest = fals
     throw new Error('Both users and carts parameters must be greater than 0.');
   }
 
-  // Primero generamos los usuarios
+  // generacion de usuario
   const users = [];
   for (let i = 0; i < numUsers; i++) {
     const user = {
@@ -15,38 +15,41 @@ async function generateMockUsers(numUsers = 0, numCarts = 0, isGetRequest = fals
       last_name: faker.person.lastName(),
       email: faker.internet.email(),
       age: faker.number.int({ min: 18, max: 60 }),
-      password: "coder123",  // Contraseña en texto plano
+      password: "coder123",  // Contraseña en texto plano (se encripta automaticamente por el model)
       role: Math.random() < 0.5 ? "user" : "admin",
     };
 
-    // Creamos el carrito para cada usuario
+    
     const cart = new Cart({ products: [] });
     if (!isGetRequest) {
-      // Si no es una petición GET, guardamos el carrito en la base de datos
+   
       await cart.save();
-      user.cart = cart._id; // Asignamos el carrito al usuario
+      user.cart = cart._id; 
     } else {
-      user.cart = cart; // En GET no guardamos el carrito en DB
+      user.cart = cart; 
     }
 
     users.push(user);
   }
 
-  // Si necesitamos más carritos, creamos los adicionales
-  const remainingCarts = numCarts - numUsers; // Cálculo de cuántos carritos adicionales necesitamos
+
+  const remainingCarts = numCarts - numUsers; // A;adi la parte del calculo porque al usuario ya crearse con un carrito me pasaba que si en el body pedia 3 carts creaba 4 
   const additionalCarts = [];
   for (let i = 0; i < remainingCarts; i++) {
     const cart = new Cart({ products: [] });
     if (!isGetRequest) {
-      await cart.save(); // Guardamos los carritos adicionales solo si es una petición POST
+      await cart.save(); 
     }
-    additionalCarts.push(cart._id); // Solo guardamos los ObjectIds
+    additionalCarts.push(cart._id); 
   }
 
-  return { users, additionalCarts }; // Devolvemos usuarios con carritos y los carritos adicionales
+  return { users, additionalCarts }; 
 }
 
 module.exports = { generateMockUsers };
+
+
+//Segundo commit para dejar las notaciones mas claras 
 
 
 
